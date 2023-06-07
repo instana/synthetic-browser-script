536b5a73bcb0d395f47343836f009fdef1e0ef71
LI JING MU <lijingmu@cn.ibm.com>
2023-06-07 17:16:01 +0800

    fix version issue

--- examples/bundledscripts/mytest.js
+++ examples/bundledscripts/mytest.js
@@ -1,3 +1,4 @@
+const assert = require("assert").strict;
 let { testBing } = require("./lib/myscript.js");
 
 console.log("pop:", $synthetic.pop);
@@ -5,4 +6,5 @@ console.log("description:", $synthetic.description);
 console.log("timezone:", $synthetic.timeZone);
 console.log("labels:", $synthetic.labels);
 console.log("Start to test");
+assert.equal($secure.username, 'user1');
 testBing();
