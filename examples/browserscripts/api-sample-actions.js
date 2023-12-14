1d2ffa9bde4bb4d006b5cf395421a762d58fda90
LI JING MU <lijingmu@cn.ibm.com>
2023-12-14 15:54:44 +0800

    update example

--- examples/browserscripts/api-sample-actions.js
+++ examples/browserscripts/api-sample-actions.js
@@ -1,38 +1,31 @@
 let click = async (message, by, timeout = 60000) => {
-    console.log(`Click on ${message} >> ${by} << `);
-    try {
-        const element = await $browser.waitForAndFindElement(by, timeout);
-        const out = $browser.actions().move({origin: element}).press().release().perform();
-        return out;
-    } catch (err) {
-        console.error(`\ncatch(click): ${err.message}`);
-        await $browser.takeScreenshot();
-        throw err;
-    }
-}
+  console.log(`Click on ${message} >> ${by} << `);
+  try {
+    const element = await $browser.waitForAndFindElement(by, timeout);
+    return $browser.actions().move({ origin: element }).click().perform();
+  } catch (err) {
+    console.error(`\ncatch(click): ${err.message}`);
+    throw err;
+  }
+};
 
 let hover = async (message, by, timeout = 60000) => {
-    console.log(`Hover on ${message} >> ${by} << `);
-    try {
-        const element = await $browser.waitForAndFindElement(by, timeout);
-        //const out = $browser.actions().move({origin: element, duration: 2000}).perform();
-        const out = $browser.actions().mouseMove(element).perform();
-        return out;
-    } catch (err) {
-        console.error(`\ncatch(hover): ${err.message}`);
-        await $browser.takeScreenshot();
-        throw err;
-    }
-}
+  console.log(`Hover on ${message} >> ${by} << `);
+  try {
+    const element = await $browser.waitForAndFindElement(by, timeout);
+    return $browser.actions().move({ origin: element, duration: 3000 }).perform();
+  } catch (err) {
+    console.error(`\ncatch(hover): ${err.message}`);
+    throw err;
+  }
+};
 
-(
-    async function () {
-        console.log(">>>>>>>>>>>>>>>>>>>", "Access bing page");
-        await $browser.get("http://www.bing.com");
-        console.log(">>>>>>>>>>>>>>>>>>>", "Actions of search");
-        await hover("search image", $driver.By.xpath("//img[@id='sbi_b']"));
-        await $browser.sleep(3000);
-        await $browser.actions().sendKeys("synthetic").perform();
-        await click("search button", $driver.By.xpath("//label[@id='search_icon']"));
-    }
-)();
+(async function () {
+  console.log(">>>>>>>>>>>>>>>>>>>", "Access bing page");
+  await $browser.get("http://www.bing.com");
+  console.log(">>>>>>>>>>>>>>>>>>>", "Actions of search");
+  await hover("search button", $driver.By.xpath("//label[@id='search_icon']"), 10000);
+  await $browser.takeScreenshot();
+  await $browser.actions().sendKeys("synthetic").perform();
+  await click("search button", $driver.By.xpath("//label[@id='search_icon']"), 10000);
+})();
