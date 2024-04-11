6b4d51e2d85beaa8c15e13eb8e857f6f8710a773
LI JING MU <lijingmu@cn.ibm.com>
2024-04-11 14:52:16 +0800

    support MFA

--- dist/src/bhelper/collect-logging.d.ts
+++
@@ -1,4 +0,0 @@
-import { Har } from "har-format";
-import { ThenableWebDriver } from "selenium-webdriver";
-import { HisEntry } from "../common/his-entry";
-export declare function collectLogging(browserType: string, headless: boolean, webdriver: ThenableWebDriver, flush: boolean): Promise<HisEntry[]> | Promise<Har>;
